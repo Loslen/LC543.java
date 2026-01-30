5fbc9618851d89eaff87133f255e5916e66af23a
Loslen <113829585+Loslen@users.noreply.github.com>
2026-01-30 22:30:43 +0800

    Create LC543.java

---
+++ LC543.java
@@ -0,0 +1,17 @@
+class Solution {
+    private int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode node) {
+        if (node == null) {
+            return -1;//葉子にとっては、鎖の長さは　-1+1=0
+        }
+        int lLen = dfs(node.left) + 1;
+        int rLen = dfs(node.right) + 1;
+        ans = Math.max(ans, lLen + rLen);
+        return Math.max(lLen,rLen);
+    }
+}
