f0165b3d19adf35db76de14e8ece0f4dfc723d94
Loslen <113829585+Loslen@users.noreply.github.com>
2026-02-05 00:06:15 +0800

    Update LC543.java

--- LC543.java
+++ LC543.java
@@ -7,7 +7,8 @@ class Solution {
 
     private int dfs(TreeNode node) {
         if (node == null) {
-            return -1;//葉子にとっては、鎖の長さは　-1+1=0
+            return -1;
+            //葉子にとっては、鎖の長さは　-1+1=0
         }
         int lLen = dfs(node.left) + 1;
         int rLen = dfs(node.right) + 1;
